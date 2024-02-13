8d5193528a8d7816bdf977571efc648870f3aac4
Kishore Sohanda <kishoresohanda100@gmail.com>
2024-02-13 10:47:54 +0530

    ChatApp project now linked with firebase CloudFireStore database, basic setup for making this application is done and screens are added with some of the logic in there.

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,41 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ChatApp'),
+      ),
+      body: StreamBuilder(
+        builder: (context, streamSnapshot) {
+          if (streamSnapshot.connectionState == ConnectionState.waiting) {
+            const Center(
+              child: CircularProgressIndicator.adaptive(),
+            );
+          }
+          final documents = streamSnapshot.data!.docs;
+          return ListView.builder(
+            itemBuilder: (context, index) => Container(
+              padding: const EdgeInsets.all(8),
+              child: Text(
+                documents[index]['text'],
+              ),
+            ),
+            itemCount: documents.length,
+          );
+        },
+        stream: FirebaseFirestore.instance
+            .collection('chats/A4PdJBakTx0lAVCTymXG/messages')
+            .snapshots(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
